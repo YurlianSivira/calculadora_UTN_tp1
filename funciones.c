43db41691941677a6a1624390d182f230ff9066d
Yurlian Sivira <siviray@gmail.com>
2018-04-13 19:14:23 -0300

    se documentan las funciones y modifica la pregunta de la opcion en el menu

--- funciones.c
+++ funciones.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+/** \brief Funcion que suma los valores enteros o decimales ingresados por el usuario
+ *
+ * \param valor1 float es el primer valor ingresado por el usuario en la funcion principal
+ * \param valor2 float es el segundo valor ingresado por el usuario en la funcion principal
+ * \return float devuelve la sumatoria de los dos valores ingresados
+ *
+ */
 float sumar(float valor1, float valor2)
 {
     float resultadoSuma;
@@ -10,6 +19,13 @@ float sumar(float valor1, float valor2)
     return resultadoSuma;
 }
 
+/** \brief funcion que resta los valores ingresados por el usuario sean enteros o decimales
+ *
+ * \param valor1 float es el primer valor ingresado por el usuario en la funcion principal
+ * \param valor2 float es el segundo valor ingresado por el usuario en la funcion principal
+ * \return float devuelve la resta o sustraccion entre los dos valores ingresados por el usuario
+ *
+ */
 float restar(float valor1, float valor2)
 {
     float resultadoResta;
@@ -19,6 +35,13 @@ float restar(float valor1, float valor2)
     return resultadoResta;
 }
 
+/** \brief funcion que permite dividir los numeros ingresados por el usuario sean decimales o enteros
+ *
+ * \param valor1 float es el primer valor ingresado por el usuario en la funcion principal
+ * \param valor2 float es el segundo valor que ingresa el usuario y es usado para verificar que sea mayor a 0
+ * \return float una vez verificado que el valor2 sea mayor a cero devuelve el resultado de lo contrario devuelve un mensaje de error
+ *               que indica que para poder efectuar la operacion el numero divisor debe ser mayor a 0
+ */
 float dividir(float valor1,float valor2)
 {
     float resultadoDividir;
@@ -28,6 +51,13 @@ float dividir(float valor1,float valor2)
     return resultadoDividir;
 }
 
+/** \brief funcion que permite multiplicar los dos valores ingresados por el usuario
+ *
+ * \param valor1 float es el primer valor ingresado por el usuario en la funcion principal
+ * \param valor2 float es el segundo valor ingresado por el usuario en la funcion principal
+ * \return float devuelve el producto de los dos numeros ingresados
+ *
+ */
 float multiplicar(float valor1, float valor2)
 {
     float resultadoMultiplicacion;
@@ -37,6 +67,12 @@ float multiplicar(float valor1, float valor2)
     return resultadoMultiplicacion;
 }
 
+/** \brief funcion que permite calcular el factorial del primer valor ingresado por el usuario (valor1)
+ *
+ * \param valor1 int toma la parte entera del valor ingresado por el usuario en la funcion principal para hacer el calculo
+ * \return long int devuelve el producto de los numeros entre el 1 y el valor ingresado por el usuario
+ *
+ */
 long int factorial(int valor1)
 {
     long int resultadoFactorial;
--- main.c
+++ main.c
@@ -2,17 +2,19 @@
 #include <stdlib.h>
 #include "funciones.h"
 
+/*Funcion principal donde se llamaran todas las funciones del programa */
 int main()
 {
-    char seguir = 's';
-    int opcion;
-    float valor1;
-    float valor2;
-    float calculo;
-    long int calculoFactorial;
+    /*DEFINO LAS VARIABLES A USAR*/
+    char seguir = 's'; /*variable usada en el while para comparar la condicion en la opcion 9*/
+    int opcion; /*almacena el numero de la opcion ingresado por el usuario */
+    float valor1; /*se usa para almacenar el valor del primer operando */
+    float valor2; /*se usa para almacenar el valor del segundo operando */
+    float calculo; /*se empleara para almacenar la respuesta de cada una de las opciones del menu */
+    long int calculoFactorial; /*variable para almacenar el valor de la operacion factorial */
 
 
-    while(seguir == 's')
+    while(seguir == 's')/*se pregunta si varia la condicion y se sigue ejecutando el bucle hasta que el usuario ingresa la opcion 9*/
     {
 
     printf("\n\n");
@@ -34,84 +36,84 @@ int main()
     printf("  ******                                          9- Salir                                                      ******\n");
     printf("  ********************************************************************************************************************\n");
     printf("\n");
-    printf("Elija una opcion del 1 al 9: ");
-    scanf("%d", &opcion);
+    printf("Elija una opcion del 1 al 9 y presione la tecla enter: ");
+    scanf("%d", &opcion); /*almacena la opcion seleccionada */
 
-        switch(opcion)
+        switch(opcion) /*se pregunta la opcion que desea efectuar el usuario */
         {
-            case 1:
-                system("cls");
+            case 1:/*PREGUNTO Y CAPTURO EL 1ER NUMERO A OPERAR*/
+                system("cls"); /*limpio la pantalla*/
                 printf("\n Ingrese el primer numero: ");
-                scanf("%f",&valor1);
+                scanf("%f",&valor1); /*se captura el primero de los valores con los que desea trabajar la persona */
                 break;
 
-            case 2:
-                system("cls");
+            case 2:/*PREGUNTO Y CAPTURO EL 2DO NUMERO A OPERAR*/
+                system("cls");/*limpio la pantalla*/
                 printf("\n Ingrese el segundo numero: ");
-                scanf("%f",&valor2);
+                scanf("%f",&valor2); /* se captura y almacena el segundo valor con el que desea trabajar la persona */
                 break;
 
-            case 3:
+            case 3:/*FUNCION SUMAR*/
                 printf("*****************************************************************************************\n");
                 printf("*****************************************************************************************\n");
 
-                calculo = sumar(valor1, valor2);
-                printf("El resultado de la suma es: %.2f", calculo);
+                calculo = sumar(valor1, valor2); /* se llama la funcion sumar pasandole los valores ingresados por el usuario como parametro */
+                printf("El resultado de la suma es: %.2f", calculo); /*se imprime el resultado de la operacion*/
 
                 printf("\n*****************************************************************************************\n");
                 printf("*****************************************************************************************\n");
                 break;
 
-            case 4:
+            case 4:/*FUNCION RESTAR*/
                 printf("*****************************************************************************************\n");
                 printf("*****************************************************************************************\n");
 
-                calculo = restar(valor1, valor2);
-                printf("El resultado de la resta es: %.2f", calculo);
+                calculo = restar(valor1, valor2); /* se llama la funcion restar pasandole los valores ingresados por el usuario como parametro */
+                printf("El resultado de la resta es: %.2f", calculo); /*se imprime el resultado de la operacion*/
 
                 printf("\n*****************************************************************************************\n");
                 printf("*****************************************************************************************\n");
                 break;
 
-            case 5:
+            case 5: /*FUNCION DIVIDIR*/
                 printf("*****************************************************************************************\n");
                 printf("*****************************************************************************************\n");
 
-                calculo = dividir(valor1, valor2);
-                if(valor2 == 0)
+                calculo = dividir(valor1, valor2); /* se llama la funcion dividir pasandole los valores ingresados por el usuario como parametro */
+                if(valor2 == 0) /*se evalua que el segundo valor ingresado por el usuario sea menor o igual  a 0 */
                 {
-                    printf("Error, para efectuar la operacion el divisor debe ser mayor a 0");
-                }else
+                    printf("Error, para efectuar la operacion el divisor debe ser mayor a 0"); /*se imprime el mensaje de error de la evaluacion*/
+                }else /*en caso de que valor2 sea mayor a 0 */
                 {
-                    printf("El resultado de la division es: %.2f", calculo);
+                    printf("El resultado de la division es: %.2f", calculo); /*se imprime el resultado de la operacion*/
                 }
                 printf("\n*****************************************************************************************\n");
                 printf("*****************************************************************************************\n");
                 break;
 
-            case 6:
+            case 6: /*FUNCION MULTIPLICAR*/
                 printf("*****************************************************************************************\n");
                 printf("*****************************************************************************************\n");
 
-                calculo = multiplicar(valor1, valor2);
-                printf("El resultado de la multiplicacion es: %.2f", calculo);
-
-                printf("\n*****************************************************************************************\n");
-                printf("*****************************************************************************************\n");
-                break;
-
-            case 7:
-                printf("*****************************************************************************************\n");
-                printf("*****************************************************************************************\n");
-
-                calculoFactorial = factorial(valor1);
-                printf("El factorial del numero es: %ld",calculoFactorial);
+                calculo = multiplicar(valor1, valor2); /* se llama la funcion multiplicar pasandole los valores ingresados por el usuario como parametro */
+                printf("El resultado de la multiplicacion es: %.2f", calculo); /*se imprime el resultado de la operacion*/
 
                 printf("\n*****************************************************************************************\n");
                 printf("*****************************************************************************************\n");
                 break;
 
-            case 8:
+            case 7: /*FUNCION FACTORIAL*/
+                printf("*****************************************************************************************\n");
+                printf("*****************************************************************************************\n");
+
+                calculoFactorial = factorial(valor1); /*se llama la funcion factorial pasandole la parte entera del primer valor ingresado por el usuario*/
+                printf("El factorial del numero es: %ld",calculoFactorial); /*se imprime el resultado de la operacion*/
+
+                printf("\n*****************************************************************************************\n");
+                printf("*****************************************************************************************\n");
+                break;
+
+            case 8: /*TODAS EN ESTE PUNTO SE EJECUTAN SIMULTANEAMENTE TODAS LAS OPERACIONES DANDO LOS RESULTADOS POR SEPARADOS Y ESPECIFICANDO CADA UNO*/
                 printf("*****************************************************************************************\n");
                 printf("*****************************************************************************************\n");
 
@@ -144,22 +146,12 @@ int main()
                 printf("*****************************************************************************************\n");
                 break;
 
-            case 9:
+            case 9: /*OPCION SALIR sale del sistema y se despide de la persona */
                 seguir = 'n';
                 printf("HASTA LUEGO");
                 break;
 
         }/*FINALIZA EL SWICTH*/
-
-    };
-
-
-
+    };/*FINALIZA EL WHILE*/
     return 0;
-
-
-
-
-
-
-}
+}/*FIN DE LA FUNCION MAIN*/
